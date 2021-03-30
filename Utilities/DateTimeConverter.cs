dd51ec05d5de8f97d26b93d57020c56697047127
Bertea Fabian <fabian.bertea@yahoo.com>
2021-03-30 13:14:16 +0300

    Created Method to convert ticks into actual date

---
+++ Utilities/DateTimeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorMvc.Utilities
+{
+    public static class DateTimeConverter
+    {
+        public static DateTime ConvertEpochToDateTime(long ticks)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(ticks);
+            DateTime dateTime = dateTimeOffset.UtcDateTime;
+            return dateTime;
+        }
+    }
+}
