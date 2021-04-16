9ece3b3590341a9a4ee629692345821cf927bc4e
Bertea Fabian <fabian.bertea@yahoo.com>
2021-04-16 13:05:00 +0300

    Shortened our code for getting enviroment variables

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -28,9 +28,9 @@ namespace RazorMvc.WebAPI.Controllers
         {
             _logger = logger;
 
-            this.lat = Environment.GetEnvironmentVariable("LATITUDE") != null ? double.Parse(Environment.GetEnvironmentVariable("LATITUDE"), CultureInfo.InvariantCulture) : double.Parse(configuration["WeatherForecast:Latitude"], CultureInfo.InvariantCulture);
-            this.lon = Environment.GetEnvironmentVariable("LONGITUDE") != null ? double.Parse(Environment.GetEnvironmentVariable("LONGITUDE"), CultureInfo.InvariantCulture) : double.Parse(configuration["WeatherForecast:Longitude"], CultureInfo.InvariantCulture);
-            this.apiKey = Environment.GetEnvironmentVariable("API_KEY") != null ? Environment.GetEnvironmentVariable("API_KEY") : configuration["WeatherForecast:ApiKey"];
+            this.lat = double.Parse(Environment.GetEnvironmentVariable("LATITUDE") ?? configuration["WeatherForecast:Latitude"], CultureInfo.InvariantCulture);
+            this.lon = double.Parse(Environment.GetEnvironmentVariable("LONGITUDE") ?? configuration["WeatherForecast:Longitude"], CultureInfo.InvariantCulture);
+            this.apiKey = Environment.GetEnvironmentVariable("API_KEY") ?? configuration["WeatherForecast:ApiKey"];
         }
 
         /// <summary>
