4fa70b0576d2f05c843826b6277bec3995e1e19b
Bertea Fabian <fabian.bertea@yahoo.com>
2021-04-19 11:01:13 +0300

    Deleted migrations

--- Startup.cs
+++ Startup.cs
@@ -50,9 +50,6 @@ namespace RazorMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(
-                    connectionString));
             services.AddDbContext<InternDbContext>(options =>
                 options.UseNpgsql(
                     connectionString));
