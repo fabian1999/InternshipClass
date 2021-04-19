4b597bb52d23c2ea0d227eb92023bfedd310e84f
Bertea Fabian <fabian.bertea@yahoo.com>
2021-04-19 14:06:17 +0300

    Refactored Seed Data to return location and intern contains any element.

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -12,28 +12,30 @@ namespace RazorMvc.Data
         public static void Initialize(InternDbContext context)
         {
             context.Database.Migrate();
-            if (context.Interns.Any())
+            if (!context.Locations.Any())
             {
-                return;   // DB has been seeded
+                var locations = new Location[]
+                {
+                defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
+                new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
+                };
+
+                context.Locations.AddRange(locations);
+                context.SaveChanges();
             }
 
-            var locations = new Location[]
+            if (!context.Interns.Any())
             {
-            defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
-            new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
-            };
-
-            context.Locations.AddRange(locations);
-
-            var interns = new Intern[]
-            {
+                var interns = new Intern[]
+                {
                 new Intern { Name = "Fabian", DateOfJoin = DateTime.Parse("2021-04-01"), Location = defaultLocation },
                 new Intern { Name = "Teo", DateOfJoin = DateTime.Parse("2021-04-01"), Location = defaultLocation },
                 new Intern { Name = "Bobi", DateOfJoin = DateTime.Parse("2021-03-31"), Location = defaultLocation },
-            };
+                };
 
-            context.Interns.AddRange(interns);
-            context.SaveChanges();
+                context.Interns.AddRange(interns);
+                context.SaveChanges();
+            }
         }
     }
 }
