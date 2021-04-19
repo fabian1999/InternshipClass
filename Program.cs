299d41b36130d3f8586ba865800ce5d9efc72170
Bertea Fabian <fabian.bertea@yahoo.com>
2021-04-19 11:41:15 +0300

    Added command line argument --recreateDb.
    We recreate only in development mode.
    
    Also added logger messages for recreating database.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,35 +13,10 @@ namespace RazorMvc
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-
-            CreateDbIfNotExists(host);
+            bool recreateDb = args.Contains("--recreateDb");
+            InitializeDb(host, recreateDb);
 
             host.Run();
-        }
-
-        private static void CreateDbIfNotExists(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<InternDbContext>();
-                    var webHostEnviroment = services.GetRequiredService<IWebHostEnvironment>();
-                    if (webHostEnviroment.IsDevelopment())
-                    {
-                        context.Database.EnsureDeleted();
-                        context.Database.EnsureCreated();
-                    }
-
-                    SeedData.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
-                }
-            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -49,5 +25,33 @@ namespace RazorMvc
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void InitializeDb(IHost host, bool recreateDb)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                try
+                {
+                    var context = services.GetRequiredService<InternDbContext>();
+                    var webHostEnviroment = services.GetRequiredService<IWebHostEnvironment>();
+                    if (webHostEnviroment.IsDevelopment() && recreateDb)
+                    {
+                        logger.LogDebug("User requested to recreate Database.");
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+                        logger.LogWarning("The Database was recreated.");
+                    }
+
+                    SeedData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
     }
 }
